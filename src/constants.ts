e3b8efa22f1c785a3f1254230afb5d0f402efc2c
Vida Xie <vida_2020@163.com>
2026-02-05 14:01:49 +0800

    fix: support `npm:` prefix semver

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const PNPM_WORKSPACE_BASENAME = 'pnpm-workspace.yaml'
 export const PACKAGE_JSON_PATTERN = `**/${PACKAGE_JSON_BASENAME}`
 export const PNPM_WORKSPACE_PATTERN = `**/${PNPM_WORKSPACE_BASENAME}`
 
-export const VERSION_TRIGGER_CHARACTERS = ['.', '^', '~', ...Array.from({ length: 10 }).map((_, i) => `${i}`)]
+export const VERSION_TRIGGER_CHARACTERS = [':', '^', '~', '.', ...Array.from({ length: 10 }).map((_, i) => `${i}`)]
 
 export const CACHE_TTL_ONE_DAY = 1000 * 60 * 60 * 24
 
--- src/providers/completion-item/version.ts
+++ src/providers/completion-item/version.ts
@@ -2,7 +2,7 @@ import type { Extractor } from '#types/extractor'
 import type { CompletionItemProvider, Position, TextDocument } from 'vscode'
 import { config } from '#state'
 import { getPackageInfo } from '#utils/api/package'
-import { parseVersion } from '#utils/package'
+import { formatVersion, parseVersion } from '#utils/package'
 import { CompletionItem, CompletionItemKind } from 'vscode'
 
 export class VersionCompletionItemProvider<T extends Extractor> implements CompletionItemProvider {
@@ -36,23 +36,21 @@ export class VersionCompletionItemProvider<T extends Extractor> implements Compl
     if (!parsed)
       return
 
-    const { prefix } = parsed
-
     const items: CompletionItem[] = []
 
-    for (const version in pkg.versionsMeta) {
-      const meta = pkg.versionsMeta[version]
+    for (const semver in pkg.versionsMeta) {
+      const meta = pkg.versionsMeta[semver]
 
       if (config.completion.version === 'provenance-only' && !meta.provenance)
         continue
 
-      const text = `${prefix}${version}`
+      const text = formatVersion({ ...parsed, semver })
       const item = new CompletionItem(text, CompletionItemKind.Value)
 
       item.range = this.extractor.getNodeRange(document, versionNode)
       item.insertText = text
 
-      const tag = pkg.versionToTag.get(version)
+      const tag = pkg.versionToTag.get(semver)
       if (tag)
         item.detail = tag
 
