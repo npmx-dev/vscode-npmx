57ebe91a4f41ae6de9fa74d81cba500f07c176f6
Vida Xie <vida_2020@163.com>
2026-02-13 16:40:10 +0800

    refactor: use `languages.match` instead of mapping

--- src/composables/active-extractor.ts
+++ src/composables/active-extractor.ts
@@ -1,25 +1,22 @@
 import type { Extractor } from '#types/extractor'
 import { PACKAGE_JSON_BASENAME, PNPM_WORKSPACE_BASENAME } from '#constants'
 import { computed, useActiveTextEditor } from 'reactive-vscode'
-import { Utils } from 'vscode-uri'
+import { languages } from 'vscode'
 import { PackageJsonExtractor } from '../extractors/package-json'
 import { PnpmWorkspaceYamlExtractor } from '../extractors/pnpm-workspace-yaml'
 
-const extractorMapping: Record<string, Extractor> = {
-  [PACKAGE_JSON_BASENAME]: new PackageJsonExtractor(),
-  [PNPM_WORKSPACE_BASENAME]: new PnpmWorkspaceYamlExtractor(),
-}
-
-export const extractorEntries = Object.entries(extractorMapping)
-  .map(([basename, extractor]) => ({ basename, pattern: `**/${basename}`, extractor }))
+export const extractorEntries = [
+  { pattern: `**/${PACKAGE_JSON_BASENAME}`, extractor: new PackageJsonExtractor() },
+  { pattern: `**/${PNPM_WORKSPACE_BASENAME}`, extractor: new PnpmWorkspaceYamlExtractor() },
+]
 
 export function useActiveExtractor() {
   const activeEditor = useActiveTextEditor()
 
-  return computed(() => {
-    const editor = activeEditor.value
-    if (!editor)
+  return computed<Extractor | undefined>(() => {
+    const document = activeEditor.value?.document
+    if (!document)
       return
-    return extractorMapping[Utils.basename(editor.document.uri)]
+    return extractorEntries.find(({ pattern }) => languages.match({ pattern }, document))?.extractor
   })
 }
