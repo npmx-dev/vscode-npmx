4275192dc0992077f8a201ee270478bbdefaa008
Vida Xie <vida_2020@163.com>
2026-02-25 16:28:00 +0800

    fix: compare prerelease correctly

--- src/providers/diagnostics/rules/upgrade.ts
+++ src/providers/diagnostics/rules/upgrade.ts
@@ -2,8 +2,9 @@ import type { DependencyInfo } from '#types/extractor'
 import type { ParsedVersion } from '#utils/version'
 import type { DiagnosticRule, NodeDiagnosticInfo } from '..'
 import { formatVersion, isSupportedProtocol, parseVersion } from '#utils/version'
-import lt from 'semver/functions/lt'
 import prerelease from 'semver/functions/prerelease'
+import gtr from 'semver/ranges/gtr'
+import ltr from 'semver/ranges/ltr'
 import { DiagnosticSeverity } from 'vscode'
 
 function createUpgradeDiagnostic(dep: DependencyInfo, parsed: ParsedVersion, upgradeVersion: string): NodeDiagnosticInfo {
@@ -24,19 +25,19 @@ export const checkUpgrade: DiagnosticRule = (dep, pkg) => {
   const { semver } = parsed
   const latest = pkg.distTags.latest
 
-  if (latest && lt(semver, latest))
+  if (latest && gtr(latest, semver))
     return createUpgradeDiagnostic(dep, parsed, latest)
 
-  const currentPreId = prerelease(semver)
-  if (!currentPreId)
+  const currentPreId = prerelease(semver)?.[0]
+  if (currentPreId == null)
     return
 
   for (const [tag, tagVersion] of Object.entries(pkg.distTags)) {
     if (tag === 'latest')
       continue
-    if (prerelease(tagVersion) !== currentPreId)
+    if (prerelease(tagVersion)?.[0] !== currentPreId)
       continue
-    if (!lt(semver, tagVersion))
+    if (ltr(tagVersion, semver))
       continue
 
     return createUpgradeDiagnostic(dep, parsed, tagVersion)
