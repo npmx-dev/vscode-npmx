049ec212aff3f46a17f1984dde04602cdd7e4d54
Vida Xie <vida_2020@163.com>
2026-04-01 11:35:31 +0800

    chore: add log

--- extensions/vscode/src/utils/request.ts
+++ extensions/vscode/src/utils/request.ts
@@ -1,6 +1,7 @@
 import type { BaseLanguageClient } from '@volar/vscode'
 import type { GetResolvedDependenciesRequest } from 'npmx-shared/protocol'
 import type { Uri } from 'vscode'
+import { logger } from '#state'
 import { RequestType } from '@volar/vscode'
 import { GET_RESOLVED_DEPENDENCIES_METHOD } from 'npmx-shared/protocol'
 
@@ -11,5 +12,10 @@ const getResolvedDependenciesRequestType = new RequestType<
 >(GET_RESOLVED_DEPENDENCIES_METHOD)
 
 export async function getResolvedDependencies(client: BaseLanguageClient, uri: Uri) {
-  return client.sendRequest(getResolvedDependenciesRequestType, { uri: uri.toString() })
+  try {
+    return client.sendRequest(getResolvedDependenciesRequestType, { uri: uri.toString() })
+  } catch (err) {
+    logger.error(`Failed to resolve dependencies for ${uri.toString()}: ${String(err)}`)
+    return []
+  }
 }
