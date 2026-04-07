f87a492617f21191db920957f55c262eee68e84f
Vida Xie <vida_2020@163.com>
2026-04-07 15:41:07 +0800

    simplify

--- packages/language-service/src/plugins/version-lens.ts
+++ packages/language-service/src/plugins/version-lens.ts
@@ -1,68 +1,18 @@
 import type { CodeLens, LanguageServicePlugin, LanguageServicePluginInstance } from '@volar/language-service'
-import type { OffsetRange } from 'npmx-language-core/types'
 import type { IWorkspaceState } from '../types'
-import type { UpgradeTier } from '../utils/version'
-import { isDependencyFile } from 'npmx-language-core/utils'
+import { checkIgnored, isDependencyFile } from 'npmx-language-core/utils'
 import { REPLACE_TEXT_COMMAND } from 'npmx-shared/commands'
 import { URI } from 'vscode-uri'
 import { getConfig } from '../config'
 import { formatUpgradeVersion, resolveUpgradeTiers } from '../utils/version'
-import { resolveUpgrade } from './diagnostics/rules/upgrade'
-
-interface LenData {
-  uri: string
-  specRange: OffsetRange
-  tier?: UpgradeTier
-}
 
 export function create(workspaceState: IWorkspaceState): LanguageServicePlugin {
-  const UNKNOWN_COMMAND: CodeLens['command'] = { title: '$(question) unknown', command: '' }
-
   return {
     name: 'npmx-version-lens',
     capabilities: {
-      codeLensProvider: {
-        resolveProvider: true,
-      },
+      codeLensProvider: {},
     },
     create(context): LanguageServicePluginInstance {
-      async function resolveVersionLensCommand({ uri, specRange, tier }: LenData, range: CodeLens['range']): Promise<CodeLens['command']> {
-        const dependencies = await workspaceState.getResolvedDependencies(uri)
-        const dep = dependencies?.find(
-          (d) => d.specRange[0] === specRange[0] && d.specRange[1] === specRange[1],
-        )
-        if (!dep)
-          return UNKNOWN_COMMAND
-
-        const pkg = await dep.packageInfo()
-        if (!pkg)
-          return UNKNOWN_COMMAND
-
-        const resolvedVersion = await dep.resolvedVersion()
-        if (!resolvedVersion)
-          return UNKNOWN_COMMAND
-
-        if (tier) {
-          const formatted = formatUpgradeVersion(dep, tier.version)
-          return {
-            title: `$(arrow-up) ${formatted} (${tier.type})`,
-            command: REPLACE_TEXT_COMMAND,
-            arguments: [uri, range, formatted],
-          }
-        }
-
-        const ignoreList = await getConfig(context, 'npmx.ignore.upgrade')
-        const targetVersion = resolveUpgrade(dep, pkg, resolvedVersion, ignoreList)
-        if (!targetVersion)
-          return { title: '$(check) latest', command: '' }
-
-        return {
-          title: `$(arrow-up) ${targetVersion}`,
-          command: REPLACE_TEXT_COMMAND,
-          arguments: [uri, range, targetVersion],
-        }
-      }
-
       return {
         async provideCodeLenses(document): Promise<CodeLens[]> {
           if (!await getConfig(context, 'npmx.versionLens.enabled'))
@@ -77,46 +27,46 @@ export function create(workspaceState: IWorkspaceState): LanguageServicePlugin {
             return []
 
           const lenses: CodeLens[] = []
-          const hideWhenLatest = await getConfig(context, 'npmx.versionLens.hideWhenLatest')
+          const [hideWhenLatest, ignoreList] = await Promise.all([
+            getConfig(context, 'npmx.versionLens.hideWhenLatest'),
+            getConfig(context, 'npmx.ignore.upgrade'),
+          ])
 
           for (const dep of dependencies) {
             if (dep.resolvedProtocol !== 'npm' || dep.category === 'peerDependencies')
+              continue
+
+            const [pkg, resolvedVersion] = await Promise.all([dep.packageInfo(), dep.resolvedVersion()])
+            if (!pkg || !resolvedVersion)
               continue
 
             const range = {
               start: document.positionAt(dep.specRange[0]),
               end: document.positionAt(dep.specRange[1]),
             }
-            const baseData: LenData = { uri: document.uri, specRange: dep.specRange }
 
-            const pkg = await dep.packageInfo()
-            const resolvedVersion = await dep.resolvedVersion()
+            const tiers = resolveUpgradeTiers(pkg, resolvedVersion)
+              .map(({ type, version }) => ({ type, formatted: formatUpgradeVersion(dep, version) }))
+              .filter(({ formatted }) => !checkIgnored({ ignoreList, name: dep.resolvedName, version: formatted }))
 
-            if (pkg && resolvedVersion) {
-              const tiers = resolveUpgradeTiers(pkg, resolvedVersion)
-              if (tiers.length > 0) {
-                for (const tier of tiers) {
-                  lenses.push({
-                    range,
-                    data: { ...baseData, tier } satisfies LenData,
-                  })
-                }
-                continue
-              }
+            if (tiers.length === 0 && !hideWhenLatest) {
+              lenses.push({ range, command: { title: '$(check) latest', command: '' } })
+              continue
             }
 
-            if (hideWhenLatest)
-              continue
-
-            lenses.push({ range, data: baseData })
+            for (const { type, formatted } of tiers) {
+              lenses.push({
+                range,
+                command: {
+                  title: `$(arrow-up) ${formatted} (${type})`,
+                  command: REPLACE_TEXT_COMMAND,
+                  arguments: [document.uri, range, formatted],
+                },
+              })
+            }
           }
 
           return lenses
-        },
-
-        async resolveCodeLens(lens): Promise<CodeLens> {
-          const command = await resolveVersionLensCommand(lens.data as LenData, lens.range)
-          return { ...lens, command }
         },
       }
     },
