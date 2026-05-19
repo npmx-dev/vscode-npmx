736b6ee765c5793e8c24cbe53d539d0ccc4cf1f0
Vida Xie <vida_2020@163.com>
2026-05-19 12:12:38 +0800

    feat: update to v3

--- packages/language-service/src/plugins/diagnostics/rules/replacement.test.ts
+++ packages/language-service/src/plugins/diagnostics/rules/replacement.test.ts
@@ -8,9 +8,21 @@ function createReplacementContext(name: string) {
 
 describe('checkReplacement', () => {
   it('should flag when replacement found', async () => {
-    expect(await checkReplacement(createReplacementContext('left-pad'), [])).toMatchObject({
-      code: 'replacement',
-    })
+    const result = await checkReplacement(createReplacementContext('left-pad'), [])
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "code": "replacement",
+        "codeDescription": {
+          "href": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart",
+        },
+        "message": ""left-pad" can be replaced with String.prototype.padStart, available since Node 8.0.0.",
+        "range": [
+          0,
+          8,
+        ],
+        "severity": 2,
+      }
+    `)
   })
 
   it('should not flag when no replacement found', async () => {
