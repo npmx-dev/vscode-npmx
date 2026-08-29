1b76f8643e70959ecbe0a42fff2aa998b938c316
Vida Xie <vida_2020@163.com>
2026-08-29 04:05:52 +0000

    refactor: dedupe shared protocol type (#155)

--- packages/shared/src/protocol.ts
+++ packages/shared/src/protocol.ts
@@ -1,13 +1,17 @@
 /* eslint-disable ts/no-namespace */
 
-import type { DependencyInfo } from 'npmx-language-core/workspace'
-
 export const GET_RESOLVED_DEPENDENCIES_METHOD = 'npmx/getResolvedDependencies'
 
 export namespace GetResolvedDependenciesRequest {
+  export interface Dependency {
+    protocol: string | null
+    resolvedSpec: string
+    specRange: [start: number, end: number]
+  }
+
   export interface ParamsType {
     uri: string
   }
-  export type ResponseType = DependencyInfo[] | undefined
+  export type ResponseType = Dependency[] | undefined
   export type ErrorType = never
 }
